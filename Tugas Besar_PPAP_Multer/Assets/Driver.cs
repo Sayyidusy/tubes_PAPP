97980ebd63fca9312efdee43d917a8084df1f98d
Ghivalza Harviansyah <hrvnsyh.ghivalza@gmail.com>
2023-06-12 13:19:39 +0700

    setir pada mobil

--- Tugas Besar_PPAP_Multer/Assets/Driver.cs
+++ Tugas Besar_PPAP_Multer/Assets/Driver.cs
@@ -6,12 +6,10 @@ public class Driver : MonoBehaviour
 {
     public Transform PlayerController; // Transform dari objek yang ingin diikuti
     public float smoothness = 5f; // Kelembutan pergerakan objek
-
     private void Update()
     {
         // Dapatkan posisi yang diinginkan untuk objek mengikuti pemain
         Vector3 targetPosition = PlayerController.position;
-
         // Interpolasi ke posisi yang diinginkan dengan kelembutan tertentu
         transform.position = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
     }
---
+++ Tugas Besar_PPAP_Multer/Assets/Scripts/StirController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StirController : MonoBehaviour
+{
+   public Transform carBody; // Transform dari bodi mobil
+    public float rotationSpeed = 100f; // Kecepatan rotasi stir
+
+    private float maxWheelRotation = 180f; // Maksimum rotasi stir dalam derajat
+
+    private void Update()
+    {
+        // Dapatkan input rotasi mobil
+        float rotationInput = Input.GetAxis("Horizontal");
+
+        // Hitung rotasi stir berdasarkan input rotasi mobil
+        float wheelRotation = rotationInput * maxWheelRotation;
+
+        // Rotasi stir
+        transform.localRotation = Quaternion.Euler(0f, 0f, -wheelRotation);
+
+        // Rotasi bodi mobil berdasarkan input rotasi mobil
+        Quaternion carRotation = Quaternion.Euler(0f, rotationInput * rotationSpeed * Time.deltaTime, 0f);
+        carBody.rotation *= carRotation;
+    }
+}
